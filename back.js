ce1d299d8618a9b3761275b261bdce0495909c62
aljaarir <133610949+aljaarir@users.noreply.github.com>
2023-10-29 17:19:00 -0400

    back.js

--- back.js
+++ back.js
@@ -33,35 +33,36 @@ setInterval(checkAndClearOnSunday, 1000 * 60 * 60 * 24); // Check every 24 hours
 
 // Function to authenticate the user
 function authenticateUser(username, password) {
-  const validUsername = 'MSUFCU';
-  const validPassword = 'Intern2023';
+  const validUsername = "MSUFCU";
+  const validPassword = "Intern2023";
 
   return username === validUsername && password === validPassword;
 }
 
 // Function to handle login button click
-document.getElementById('loginButton').addEventListener('click', function () {
-  const username = document.getElementById('username').value;
-  const password = document.getElementById('password').value;
-  const loginStatus = document.getElementById('loginStatus');
-  const tableContainer = document.getElementById('table-container');
-  const loginContainer = document.getElementById('login-container');
-  const showPasswordCheckbox = document.getElementById('showPassword');
+document.getElementById("loginButton").addEventListener("click", function () {
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+  const loginStatus = document.getElementById("loginStatus");
+  const tableContainer = document.getElementById("table-container");
+  const loginContainer = document.getElementById("login-container");
+  const showPasswordCheckbox = document.getElementById("showPassword");
   const checkboxes = document.querySelectorAll('input[type="checkbox"]');
 
   if (authenticateUser(username, password)) {
-    loginStatus.textContent = 'Login successful.';
-    tableContainer.classList.remove('blur'); // Remove blur
-    loginContainer.style.display = 'none'; // Hide the login form
-    showPasswordCheckbox.style.display = 'none'; // Hide the Show Password checkbox
+    loginStatus.textContent = "Login successful.";
+    tableContainer.classList.remove("blur"); // Remove blur
+    loginContainer.style.display = "none"; // Hide the login form
+    showPasswordCheckbox.style.display = "none"; // Hide the Show Password checkbox
     // Enable checkboxes
     checkboxes.forEach((checkbox) => {
       checkbox.disabled = false;
     });
   } else {
-    loginStatus.textContent = 'Invalid username or password. Please refresh the page and try again.';
+    loginStatus.textContent =
+      "Invalid username or password. Please refresh the page and try again.";
     // Add blur and disable checkboxes
-    tableContainer.classList.add('blur');
+    tableContainer.classList.add("blur");
   }
 });
 
@@ -74,13 +75,12 @@ function saveCheckboxStates() {
     checkboxStates[`checkbox-${index}`] = checkbox.checked;
   });
 
-  localStorage.setItem('checkboxStates', JSON.stringify(checkboxStates));
+  localStorage.setItem("checkboxStates", JSON.stringify(checkboxStates));
 }
-
 
 // Restore checkbox states when the page loads
 function restoreCheckboxStates() {
-  const checkboxStates = JSON.parse(localStorage.getItem('checkboxStates'));
+  const checkboxStates = JSON.parse(localStorage.getItem("checkboxStates"));
 
   if (checkboxStates) {
     const checkboxes = document.querySelectorAll('input[type="checkbox"]');
@@ -96,25 +96,31 @@ function restoreCheckboxStates() {
 restoreCheckboxStates(); // Restore checkbox states when the page loads
 
 // Save checkbox states when any checkbox changes
-document.querySelectorAll('input[type="checkbox"]').forEach((checkbox, index) => {
-  checkbox.addEventListener('change', () => {
-    saveCheckboxStates();
+document
+  .querySelectorAll('input[type="checkbox"]')
+  .forEach((checkbox, index) => {
+    checkbox.addEventListener("change", () => {
+      saveCheckboxStates();
+    });
   });
-});
 
-// Auto-save checkbox states every 2 seconds
+// Auto-save checkbox states every 0.00005 seconds
 setInterval(saveCheckboxStates, 1000 * 0.00005);
 
 // Function to handle clicking the first element (text) in a row and handle "Select All"
 function handleTextClick(event) {
   const target = event.target;
-  if (target.tagName === 'TD' && target.parentElement) {
-    const firstCheckbox = target.parentElement.querySelector('input[type="checkbox"]');
+  if (target.tagName === "TD" && target.parentElement) {
+    const firstCheckbox = target.parentElement.querySelector(
+      'input[type="checkbox"]'
+    );
     if (firstCheckbox) {
       firstCheckbox.checked = !firstCheckbox.checked;
 
       // Set the state of all checkboxes in the row to match the first checkbox
-      const checkboxes = target.parentElement.querySelectorAll('input[type="checkbox"]');
+      const checkboxes = target.parentElement.querySelectorAll(
+        'input[type="checkbox"]'
+      );
       checkboxes.forEach((checkbox) => {
         checkbox.checked = firstCheckbox.checked;
       });
@@ -124,4 +130,13 @@ function handleTextClick(event) {
 }
 
 // Add a click event listener to the table to handle clicking the first element (text) in a row
-document.querySelector('table').addEventListener('click', handleTextClick);
+
+const table = document.querySelector("table");
+
+table.addEventListener("click", function (event) {
+  const target = event.target;
+  if (target.tagName === "TD" && target.parentElement.firstElementChild === target) {
+    // Check if the clicked element is a TD and is the first child in the row
+    handleTextClick(event);
+  }
+});
